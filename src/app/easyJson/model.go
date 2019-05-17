b93b782cb8875304d3d291d40c397482cf8366de
Stas Yago <stasyag86@gmail.com>
2019-05-17 22:03:57 +0300

    Update model.go

--- src/app/easyJson/model.go
+++ src/app/easyJson/model.go
@@ -1,1 +1,12 @@
+package easyJson
 
+//easyjson:json
+type RequestPattern struct {
+	Domain string `json:"domain"`
+	Path   string `json:"path"`
+}
+
+//easyjson:json
+type ResponsePattern struct {
+	Location   string `json:"location"`
+}
